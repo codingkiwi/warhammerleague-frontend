798612588a53701ae87c6959cb54c0a7d163a5ff
codingkiwi <justin.reti@gmail.com>
2020-11-16 20:11:29 +1300

    added full frontend functionaility for basic mern REACT scaffold

--- src/App.js
+++ src/App.js
@@ -1,8 +1,71 @@
 
+import React from 'react'; //import Suspense if using lazy loading below
+import {
+	BrowserRouter as Router,
+	Route,
+	Redirect,
+	Switch,
+} from 'react-router-dom';
+
+import Auth from './user/pages/Auth';
+import MainNavigation from './shared/components/Navigation/MainNavigation';
+import { AuthContext } from './shared/context/auth-context';
+import { useAuth } from './shared/hooks/auth-hook';
+
+
 const App = () => {
-  return (
-    <p>Main app content</p>
-  );
+  const { userId, token, login, logout } = useAuth();
+
+	let routes;
+
+	if (token) {
+		routes = (
+			<Switch>
+				<Route path='/' exact>
+					<p>Logged In Home</p>
+				</Route>
+				<Redirect to='/' />
+			</Switch>
+		);
+	} else {
+		routes = (
+			<Switch>
+				<Route path='/' exact>
+					<p>Logged Out Home</p>
+				</Route>
+				<Route path='/auth' exact>
+					<Auth />
+				</Route>
+				<Redirect to='/auth' />
+			</Switch>
+		);
+	}
+	return (
+		<AuthContext.Provider
+			value={{
+				isLoggedIn: !!token,
+				token: token,
+				userId: userId,
+				login: login,
+				logout: logout,
+			}}
+		>
+			<Router>
+				<MainNavigation />
+				<main>
+					{/* <Suspense
+						fallback={
+							<div className='center'>
+								<LoadingSpinner />
+							</div>
+						}
+					> */}
+						{routes}
+					{/* </Suspense> */}
+				</main>
+			</Router>
+		</AuthContext.Provider>
+	);
 }
 
 export default App;
