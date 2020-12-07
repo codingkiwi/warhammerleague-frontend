c937671f7c54490dd9788fb8c0023f52b87769ce
codingkiwi <justin.reti@gmail.com>
2020-12-07 15:48:12 +1300

    added basic fetching for leagues and league details

--- src/league/pages/LeagueDetails.js
+++ src/league/pages/LeagueDetails.js
@@ -1,11 +1,37 @@
-import React from 'react';
-import {useParams} from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+
+import { useHttpClient } from '../../shared/hooks/http-hook';
+import LoadingSpinner from '../../shared/components/UIElements/LoadingSpinner';
 
 import './LeagueDetails.css';
 
 const LeagueDetails = (props) => {
-    const leagueId = useParams().leagueId;
-return <p>{leagueId}</p>;
+	const { isLoading, error, sendRequest, clearError } = useHttpClient();
+
+	const leagueId = useParams().leagueId;
+	const [leagueDetails, setLeagueDetails] = useState();
+
+	useEffect(() => {
+		const fetchLeagues = async () => {
+			try {
+				const responseData = await sendRequest(
+					process.env.REACT_APP_BACKEND_URL + '/leagues/' + leagueId
+				);
+				console.log(responseData.league);
+				setLeagueDetails(responseData.league);
+			} catch (err) {
+				console.log(err.message);
+			}
+		};
+		fetchLeagues();
+	}, [sendRequest, leagueId]);
+
+	if (!leagueDetails) {
+		return <p>no league details</p>;
+	} else {
+		return <p>{leagueDetails.name}</p>;
+	}
 };
 
 export default LeagueDetails;
--- src/league/pages/LeagueList.js
+++ src/league/pages/LeagueList.js
@@ -1,9 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+
+import { useHttpClient } from '../../shared/hooks/http-hook';
 
 import './LeagueList.css';
 
 const LeagueList = (props) => {
-	return <p>leaguelist</p>;
+	const { isLoading, error, sendRequest, clearError } = useHttpClient();
+	const [leagueList, setLeagueList] = useState([]);
+
+	useEffect(() => {
+		const fetchLeagues = async () => {
+			try {
+				const responseData = await sendRequest(
+					process.env.REACT_APP_BACKEND_URL + '/leagues'
+				);
+				console.log(responseData);
+				setLeagueList(responseData.leagues);
+			} catch (err) {
+				console.log(err.message);
+			}
+		};
+		fetchLeagues();
+	}, [sendRequest]);
+
+	return leagueList.map((league) => (
+		<ul>
+			<li>{league.name}</li>
+			<li>{league.description}</li>
+			<Link to={'/leagues/' + league.id}>Details</Link>
+		</ul>
+	));
 };
 
 export default LeagueList;
