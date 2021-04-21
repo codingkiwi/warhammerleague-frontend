030919cb73c1c9d53c082cde99da3e2c83f5eebf
codingkiwi <justin.reti@gmail.com>
2021-04-21 17:07:41 +1200

    fixed font style import bug and tweaked styling to homepage. Changed homepage link to a button component

--- src/home/pages/Home.js
+++ src/home/pages/Home.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
 import Card from '../../shared/components/UIElements/Card';
+import Button from '../../shared/components/FormElements/Button';
 
 import './Home.css';
 
@@ -11,12 +11,7 @@ const Home = () => {
 			<div className='home-container-hero'>
 				<h1>40k Ranked Ladder Manager</h1>
 				<h2>Create and manage ranked ladders for Warhammer 40k</h2>
-				<h3>
-					Compete against your friends for rankings. Watch your local
-					meta evolve. Work your way up the ladder and prove who's
-					number one
-				</h3>
-				<Link to='/auth'>Get Started</Link>
+				<Button to='/auth'>Get Started</Button>
 			</div>
 			<div className='home-container-light'>
 				<Card>
@@ -25,11 +20,18 @@ const Home = () => {
 				</Card>
 				<Card>
 					<h5>Improve Your Ranking</h5>
-					<p>Gain or Lose ranking points depending on you and your opponents relative ranking using the ELO system.</p>
+					<p>
+						Compete against your friends for rankings. Gain or Lose
+						ranking points depending on you and your opponents
+						relative ranking using the ELO system.
+					</p>
 				</Card>
 				<Card>
 					<h5>View Your Stats</h5>
-					<p>Coming soon...</p>
+					<p>
+						Watch your local meta evolve. This feature is coming
+						soon...
+					</p>
 				</Card>
 			</div>
 		</div>
