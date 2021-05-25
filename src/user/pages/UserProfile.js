caae13da3b62e2440910bed3e8b4463d56bf50ec
codingkiwi <justin.reti@gmail.com>
2021-05-25 14:33:51 +1200

    commented out user profile code to stop errors for push

--- src/user/pages/UserProfile.js
+++ src/user/pages/UserProfile.js
@@ -1,14 +1,14 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import { useParams } from 'react-router-dom';
 
 import './UserProfile.css';
 
-import ErrorModal from '../../shared/components/UIElements/ErrorModal';
-import LoadingSpinner from '../../shared/components/UIElements/LoadingSpinner';
-import { AuthContext } from '../../shared/context/auth-context';
+// import ErrorModal from '../../shared/components/UIElements/ErrorModal';
+// import LoadingSpinner from '../../shared/components/UIElements/LoadingSpinner';
+// import { AuthContext } from '../../shared/context/auth-context';
 
 const UserProfile = (props) => {
-	const auth = useContext(AuthContext);
+	// const auth = useContext(AuthContext);
 	const userId = useParams().userId;
 
     console.log(userId);
