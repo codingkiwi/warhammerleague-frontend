54a460a1fa1b482bee3390aba7f2ded8dda4940e
codingkiwi <justin.reti@gmail.com>
2022-05-04 11:39:28 +1200

    restyled homepage

--- src/home/pages/Home.js
+++ src/home/pages/Home.js
@@ -2,18 +2,28 @@ import React from 'react';
 
 import Card from '../../shared/components/UIElements/Card';
 import Button from '../../shared/components/FormElements/Button';
+import texture from '../../Images/always-grey.png';
 
 import './Home.css';
 
 const Home = () => {
 	return (
-		<div className='main-container'>
+		<div className='home-container'>
 			<div className='home-container-hero'>
 				<h1>40k Ranked Ladder Manager</h1>
-				<h2>Create and manage ranked ladders for Warhammer 40k</h2>
+				<h3>Create and manage ranked ladders for Warhammer 40k</h3>
 				<Button to='/auth'>Get Started</Button>
 			</div>
-			<div className='home-container-light'>
+			<div className='home-container-subhero' style={{backgroundImage:`url(${texture})`}}>
+				<div className='home-subhero-contents'>
+					<h2>Enjoy 40k more with <span className='accent-text'>friendly competition</span></h2>
+					<h3>
+						Foster friendly competition and rivalries. Keep a record
+						of your conquests. Get your local community engaged
+					</h3>
+				</div>
+			</div>
+			<div className='home-container-dark'>
 				<Card>
 					<h5>Create a Ladder</h5>
 					<p>Create a ranked ladder and invite your friends.</p>
--- src/shared/components/UIElements/Card.js
+++ src/shared/components/UIElements/Card.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import texture from '../../../Images/always-grey.png'
 
 import './Card.css';
 
 const Card = props => {
   return (
-    <div className={`card ${props.className}`} style={props.style}>
+    <div className={`card ${props.className}`} style={props.style || {backgroundImage:`url(${texture})`}}>
       {props.children}
     </div>
   );
